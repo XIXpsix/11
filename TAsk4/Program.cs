922dee9ea33b59b96de5002d4062eff86334854e
XIXpsix <lebedevdima902@gmail.com>
2024-12-10 11:16:01 +0200

    Add files via upload

---
+++ TAsk4/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TAsk4
+{
+    /// <summary>
+    /// Задача 4. Сортировка массива по возрастанию.
+    /// </summary>
+    class Program
+    {
+        //static void Main()
+        //{
+        //    string[] array = { "Яблоко", "Груша", "Мандарин" };
+        //    Array.Sort(array);
+        //    foreach (var fruit in array)
+        //    {
+        //        Console.WriteLine(fruit);
+        //    }
+        //}
+    }
+}
